29d95d421a0c88b209690e7b797e6b1a3abcd973
gj-citrix <gj.citrix@gmail.com>
2016-08-17 15:31:14 -0400

    add files first time
    some log related utilities

---
+++ sc-test/sc-test/CodedUITest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace sc_test
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        //public MyLogHelper myLog = new MyLogHelper(); //(System.Reflection.MethodBase.GetCurrentMethod());
+       private log4net.ILog log;
+
+        public CodedUITest1()
+        {
+            MyLogHelper myLog = new MyLogHelper(System.Reflection.MethodBase.GetCurrentMethod());
+            log = myLog.Logger;
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            log.Info("Application starts!");
+
+            //string str1 = System.Configuration.ConfigurationManager.AppSettings["k1"];
+            //MessageBox.Show(str1);
+
+            log.Warn("Calling Cancel!");
+
+            this.UIMap.cancelbutton();
+
+            log.Debug("Test finished running");
+
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
---
+++ sc-test/sc-test/MyLogHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace sc_test
+{
+    public class MyLogHelper
+    {
+        private static log4net.ILog m_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MyLogHelper()
+        { }
+
+        public MyLogHelper(System.Reflection.MethodBase methbase)
+        {
+            m_logger = log4net.LogManager.GetLogger(methbase.DeclaringType);
+        }
+
+        public log4net.ILog Logger
+        {
+             get { return m_logger; }
+        } 
+    }
+
+
+}
